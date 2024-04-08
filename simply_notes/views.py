ce3fcf1ae81a092917a116b192153ae61fdec2f7
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-04-08 21:29:09 +0530

    final push

--- simply_notes/views.py
+++ simply_notes/views.py
@@ -4,7 +4,11 @@ from .forms import SignupForm, LoginForm, NoteUploadForm
 from .models import UserType, Note
 from django.core.exceptions import ObjectDoesNotExist
 
+
+
+
 def home(request):
+    
     return render(request, 'home/home.html')
 
 def user_signup(request):
@@ -57,5 +61,3 @@ def dashboard(request):
 def user_logout(request):
     logout(request)
     return redirect('login')
-
-
